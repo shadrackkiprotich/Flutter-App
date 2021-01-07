8eb96c232af784a3e48ad8bac393ad2003ff98cc
shadrackkiprotich <mylesshadrack99@gmail.com>
2021-01-07 11:52:05 +0300

    Edited Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue[900],
-        title: Text("My App"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text("Hello World"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text("Click me "),
-        backgroundColor: Colors.blue[900],
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text("My App"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text("Hello World"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text("Click me "),
+          backgroundColor: Colors.blue[900],
+        ),
       ),
     ),
-  ));
+  );
 }
