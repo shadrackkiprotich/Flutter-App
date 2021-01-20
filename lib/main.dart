72b27127f036979426dbd748f275e2edf3ff35cd
shadrackkiprotich <mylesshadrack99@gmail.com>
2021-01-20 14:13:04 +0300

    Added rows and containers to the original app

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,61 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue[900],
-          title: Text("My App"),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Text("Hello World"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text("Click me "),
-          backgroundColor: Colors.blue[900],
-        ),
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        title: Text("My App"),
       ),
-    ),
-  );
+      // body: Container(
+      // padding: EdgeInsets.fromLTRB(20.0, 10.0, 40.0, 30.0),
+      //margin: EdgeInsets.all(30.0),
+      //color: Colors.green[800],
+      /* body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text("Good Morning Kenya"),
+          RaisedButton(
+            onPressed: () {},
+            color: Colors.blue,
+            child: Text("Click me"),
+          ),
+          Container(
+            child: Text("Hello world"),
+            color: Colors.amber,
+            padding: EdgeInsets.all(10.0),
+          )
+        ],
+      ),*/
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            children: <Widget>[Text("I am HGS")],
+          ),
+          Container(
+            child: Text("Hello Myles"),
+            color: Colors.deepOrange,
+          ),
+          Container(
+            child: Text("Hello Langat"),
+            color: Colors.deepPurple,
+          ),
+          Container(
+            child: Text("Hello Shadrack "),
+            color: Colors.deepPurpleAccent,
+          )
+        ],
+      ),
+    );
+  }
 }
